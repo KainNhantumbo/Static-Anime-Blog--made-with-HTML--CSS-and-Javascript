f25436ccbaa343c71615cef3172b7616ea007ab1
Kain Nhantumbo <nhantumbok@gmail.com>
2021-12-07 09:04:23 -0300

    Restruturação dos arquivos e definição das principais rotas do site.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const headerRefresh = document.querySelector('.navegacao');
+
+// headerRefresh.addEventListener('click', (e) =>{
+//     e.preventDefault();
+//     let checkbtn = document.querySelector('.checkbtn');
+
+//     checkbtn = e.target;
+//     checkbtn.classList.toggle('.list-item');
+//     console.log(checkbtn)
+// });
